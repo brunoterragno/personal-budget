bb447ce159d57bd18e4260065667da38bf9c7237
Bruno Terragno <bruno.terragno@gmail.com>
2019-04-04 00:11:53 -0300

    Add tab bar

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <TabBar />
+        <TotalWidget />
         <Card />
         <MyButton title="Title" />
       </View>
@@ -25,6 +27,23 @@ const MyButton = ({ title }) => (
   </View>
 );
 
+const TotalWidget = () => (
+  <View style={styles.totalWidgetContainer}>
+    <Text style={styles.totalWidgetText}>BALANCE</Text>
+    <Text style={styles.totalWidgetMainText}>-$171 559.80</Text>
+    <Text style={styles.totalWidgetText}>+4.28%</Text>
+  </View>
+);
+
+const TabBar = () => (
+  <View style={styles.tabBarContainer}>
+    <Text style={styles.tabBarText}>DAILY</Text>
+    <Text style={styles.tabBarText}>WEEKLY</Text>
+    <Text style={styles.tabBarTextSelected}>MONTHLY</Text>
+    <Text style={styles.tabBarText}>YEARLY</Text>
+  </View>
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -41,7 +60,7 @@ const styles = StyleSheet.create({
   cardContainer: {
     flex: 1,
     justifyContent: "center",
-    maxHeight: "8%",
+    maxHeight: "9%",
     minWidth: "80%",
     backgroundColor: "#FFFFFF",
     borderRadius: 8
@@ -64,5 +83,41 @@ const styles = StyleSheet.create({
     minWidth: "80%",
     backgroundColor: "#65BCBF",
     borderRadius: 4
+  },
+  totalWidgetContainer: {
+    flex: 1,
+    maxHeight: "9%",
+    minWidth: "80%",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  totalWidgetMainText: {
+    color: "#FFFFFF",
+    fontWeight: "600",
+    fontSize: 40
+  },
+  totalWidgetText: {
+    color: "#FFFFFF",
+    fontWeight: "500",
+    fontSize: 16,
+    opacity: 0.8
+  },
+  tabBarContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    maxHeight: "5%"
+  },
+  tabBarTextSelected: {
+    color: "#FFFF",
+    fontWeight: "bold",
+    fontSize: 13
+  },
+  tabBarText: {
+    color: "#FFFF",
+    fontWeight: "bold",
+    fontSize: 13,
+    opacity: 0.7
   }
 });
